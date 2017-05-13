882d83bc68f52c984c78d9f03c494a1fa70c4381
Raja Hasnain Anwar <rajahasnain_570@yahoo.com>
2017-05-14 00:44:41 +0500

    Add files via upload

---
+++ Hangman Game/Main.java
@@ -0,0 +1,13 @@
+package hangmangame;
+import javax.swing.*;
+
+/**
+ *
+ * @author RajaHasnain
+ */
+public class Main {
+    
+    public static void main(String[] args){
+        Game hangman = new Game();
+    }
+}
