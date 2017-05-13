7d8036952624fb0dd3ccabbe5cfdf1efd549cee2
Raja Hasnain Anwar <rajahasnain_570@yahoo.com>
2017-05-13 23:48:47 +0500

    Test class for Goldbach's Conjecture

---
+++ GoldBachsTest.java
@@ -0,0 +1,11 @@
+package goldBachs;
+
+public class GoldBachsTest {
+    public static void main(String args[]){
+        goldBachs obj = new goldBachs(50);
+        goldBachs obj2 = new goldBachs(22);
+        //start operation
+        obj.initiate();
+        obj2.initiate();
+    }
+}
