cf9f6cbc82cb850efb250ae792ec9a2bea9a9c77
Raja Hasnain Anwar <rajahasnain_570@yahoo.com>
2017-07-07 12:13:22 +0500

    Update NumberPlateRegex.java

--- NumberPlateRegex.java
+++ NumberPlateRegex.java
@@ -1,26 +1,92 @@
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class NumberPlateRegex {
-    
-    
-    public static void main( String args[] ){
-        
-        String REGEX = "^\\s*[a-zA-z]{1,4}\\s*[-]*[0-9]{0,2}\\s*[-]*[0-9]{3,4}\\s*$"; //regular expression
-        String numbers[] = {"ICT LKI-12-1245", "N864 152", "AB05 CD", "APF--5452 Punjab", "ABC123", "EKD 182", "ABC 123", "ABC 1234", "ABC1234", "he12467", "b13486894", "345abc", "sfa13saf", "_09313_alk", "2jlk0a", "jajlk 02", "1234 ABC", "abcde 1234"};
-        
-        Pattern pattern; //a pattern of compiled regex
-        Matcher matcher; //helps in matching the regex
+import com.jaunt.Element;
+import com.jaunt.Elements;
+import com.jaunt.JauntException;
+import com.jaunt.UserAgent;
+import com.jaunt.component.Form;
 
+public class NumberPlateRegex {
+
+	public static void main(String[] args) throws JauntException {
+		String REGEX = "^\\s*[a-zA-z]{1,4}\\s*[0-9]{3,4}\\s*$"; //regular expression
+        String numbers[] = {"ICT LKI-12-1245", "IDL 2121", "LXH 1050", "ICT  LS 302 ISLAMABAD",
+        					"IDL 1740", "AAK-760 ICT-ISLAMABAD", "ICT NE 410 ISLAMABAD", "ZV - 195",
+        					"b13486894", "345abc", "sfa13saf", "_09313_alk", "2jlk0a", "jajlk 02",
+        					"1234 ABC", "LWD -16-8754", "ZS -  319", "STH 374"};
+        String data = "";
+        
+        Pattern pattern, p; //a pattern of compiled regex
+        Matcher matcher, m; //helps in matching the regex
+        
         for(String number : numbers){ //for every number
             //fixing
-            Pattern p = Pattern.compile("ICT|Islamabad|Sindh|Punjab|Govt of Sindh|ICT-Islamabad");
-            Matcher m = p.matcher(  number );
+        	System.out.println("Input is " + number + "\n");
+        	m = Pattern.compile("[-][0-9]{2}[-]|[-]").matcher( number );
+            number = m.replaceAll(" ");
+            
+            p = Pattern.compile("ICT|Islamabad|Sindh|Punjab|Govt of Sindh|ICT-Islamabad|ISLAMABAD|Govt of Pakistan");
+            m = p.matcher(  number );
             number = m.replaceAll("");
+            
             
             pattern = Pattern.compile(REGEX); //compile regex
             matcher = pattern.matcher(number); //create its matcher
-            System.out.println(number + " : " + matcher.matches()); //match the input with regex
+            
+            long start = System.currentTimeMillis();
+            
+            number= number.replaceAll("( +)"," ").trim();
+            if (matcher.matches()) { //print if valid
+	            System.out.println("Searching for " + number + " ...\n"); //match the input with regex
+	            data = find( number );
+            }
+            else {
+            	System.out.println("Invalid!\n\n");
+            	continue;
+            }
+            
+            if(!data.isEmpty())
+            	   System.out.println( data ); //match the input with regex
+            else
+            	   System.out.println("No record found"); //match the input with regex
+            
+            long end = System.currentTimeMillis();
+            
+            System.out.println("TIme: " + (end - start)/1000.0 +" seconds.\n\n");
         }
-   }
+	}
+	
+	public static String find(String number) throws JauntException {
+	
+		StringBuffer string = new StringBuffer();
+		
+		UserAgent userAgent = new UserAgent();
+		userAgent.visit("http://www.mtmis.excise-punjab.gov.pk/");
+		
+		Form form = userAgent.doc.getForm(1);
+		
+		form.setTextField("vhlno", number);
+		
+		form.submit();
+		try {
+			Element table = userAgent.doc.findFirst("<table>");
+			Elements trs = table.findEach("<tr>"); //get all rows
+			
+			for(Element tr: trs){ //in each row
+				Elements tds = tr.findEach("<td>"); //get all cells
+				
+				for(Element td: tds){ // in each cell
+					String text = td.getText();
+					string.append( String.format("%" + -22 + "s", text ) );//get text
+				}
+				
+				string.append("\n");
+			}
+			
+			return string.toString();
+		} catch(com.jaunt.NotFound es) {
+			return "";
+		}
+	}
 }
